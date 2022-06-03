dbb35059350ccc2d53dd46018f325f14f0de4b2e
Jake O'Rielly <jake.orielly@gmail.com>
2022-06-02 21:36:26 -0700

    Nav Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { ShowText } from './components/ShowText'
+import { ShowText } from './components/ShowText';
+import { NavBar } from './components/NavBar';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <nav>
-          <Link to="/">{"Home Page"}</Link>
-          <Link to="/blog">{"Blog"}</Link>
-        </nav>
+      < NavBar />
         <Routes>
           <Route path="/" element={
             <ShowText text="Welcome to the home page." />
---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles(() => ({
+  root: {
+    position: 'relative',
+    width: '100%',
+    paddingBottom: '1rem',
+    borderBottom: '1px solid black',
+    height: '2rem',
+
+    "& nav": {
+      margin: 0,
+      position: 'absolute',
+      top: '50%',
+      MStransform: 'translateY(-50%)',
+      transform: 'translateY(-50%)',
+      "& a": {
+        color: 'black',
+        textDecoration: 'none',
+        margin: '0rem 0.5rem'
+      }
+    }
+  }
+}));
+
+export const NavBar = () => {
+  const classes = useStyles(); 
+
+  return (
+    <div
+      className={classes.root}
+    >
+      <nav
+      >
+        <Link to="/">{"Home Page"}</Link>
+        <Link to="/blog">{"Blog"}</Link>
+      </nav>
+    </div>
+  )
+};
+
