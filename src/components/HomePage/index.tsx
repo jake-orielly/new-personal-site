01f6fd7fdcb3a3c18d698621489c6892e469cf9e
Jake O'Rielly <jake.orielly@gmail.com>
2022-06-18 15:46:29 -0700

    Update homepage text

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,21 +1,8 @@
 export const HomePage = () => {
   return (
     <div>
-      <div>
-        I work at an awesome startup called{" "}
-        <a href="https://fieldguide.io" target="_blank" rel="noreferrer">
-          Fieldguide
-        </a>{" "}
-        (we're{" "}
-        <a
-          href="https://www.fieldguide.io/careers"
-          target="_blank"
-          rel="noreferrer"
-        >
-          hiring
-        </a>
-        ).
-      </div>
+      <div>My name is Jake O'Rielly.</div>
+      <div>I'm a software engineer based in San Francisco.</div>
     </div>
   );
 };
