777268a95155325714583be5022554a646e0526d
Jake O'Rielly <jake.orielly@gmail.com>
2022-06-20 13:46:22 -0700

    Update contact page

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,3 +1,29 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles(() => ({
+  emailText: {
+    fontSize: "1.6rem",
+    marginTop: "4rem",
+  },
+  jobText: {
+    fontSize: "1.1rem",
+    marginTop: "2rem",
+  },
+}));
+
 export const Contact = () => {
-  return <div>jake.orielly@gmail.com</div>;
+  const classes = useStyles();
+
+  return (
+    <div>
+      <p className={classes.emailText}>
+        You can email me at{" "}
+        <a href="mailto: jake.orielly@gmail.com">jake.orielly@gmail.com</a>
+      </p>
+      <p className={classes.jobText}>
+        I'm very happy at my current job and am not seeking new work at this
+        time.{" "}
+      </p>
+    </div>
+  );
 };
