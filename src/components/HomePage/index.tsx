29e5388433b2880dcf59d1a961672b638627de4a
Jake O'Rielly <jake.orielly@gmail.com>
2022-06-20 13:35:07 -0700

    Homescreen image blur up

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,5 +1,7 @@
+import { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import mountains from "./mountains.jpg";
+import mountains from "./images/mountains.webp";
+import mountainsSmall from "./images/mountains-small.webp";
 
 export const useStyles = makeStyles(() => ({
   root: {
@@ -22,12 +24,24 @@ export const useStyles = makeStyles(() => ({
       left: 0,
       width: "100%",
       boxShadow: "0px -1px 6px rgb(50 50 50 / 25%)",
+      transition: "filter 1s ease-out",
     },
   },
 }));
 
 export const HomePage = () => {
   const classes = useStyles();
+  const smallImg = mountainsSmall;
+  const fullImg = mountains;
+  const [src, setSrc] = useState(smallImg);
+  useEffect(() => {
+    setSrc(smallImg);
+    const img = new Image();
+    img.src = fullImg;
+    img.onload = () => {
+      setSrc(fullImg);
+    };
+  }, [smallImg, fullImg]);
 
   return (
     <div className={classes.root}>
@@ -36,7 +50,13 @@ export const HomePage = () => {
       <h2>Short fiction writer</h2>
       <h2>Outdoorsman</h2>
       <h3>San Francisco, CA</h3>
-      <img src={mountains} alt="" />
+      <img
+        src={src}
+        style={{
+          filter: src === smallImg ? "blur(20px)" : "none",
+        }}
+        alt=""
+      />
     </div>
   );
 };
